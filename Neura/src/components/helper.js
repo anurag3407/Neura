a548de6b866cbba95be9d04e4d508d7d6a333e3d
Anurag Mishra <anuragmishra3407@gmail.com>
2025-05-21 21:41:12 +0530

    final commit

--- Neura/src/components/helper.js
+++
@@ -1,7 +0,0 @@
-export function checkHeading(str) {
-  return str.startsWith('**') || str.endsWith('*');
-}
-
-export function replaceHeadingStars(str) {
-  return str.replace(/^\*{2}/, "").replace(/\*$/, "");
-}
